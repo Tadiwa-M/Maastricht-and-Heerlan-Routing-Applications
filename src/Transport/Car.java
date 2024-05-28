9be34d7ec779ef254ffea66301b1acfe7c27d083
Amirmohseni87 <amirmohseni87@yahoo.com>
2024-05-28 16:29:04 +0200

    Fixed bugs for car calculations

--- src/Transport/Car.java
+++ src/Transport/Car.java
@@ -3,7 +3,7 @@ package Transport;
 public class Car extends Vehicle {
     public Car(double distance) {
         super(distance);
-        this.speed = 0.5;
+        this.speed = 0.67;
         this.vehicleConstant = 1.5;
     }
 
