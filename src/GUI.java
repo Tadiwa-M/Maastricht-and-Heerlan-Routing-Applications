08a8e984f46544cce2670b0307d529f99190c58a
Amirmohseni87 <amirmohseni87@yahoo.com>
2024-06-25 22:57:52 +0200

    Added parallel computation for GUI and graph creation

--- src/GUI.java
+++ src/GUI.java
@@ -15,6 +15,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 
 public class GUI extends JFrame {
@@ -53,14 +54,19 @@ public class GUI extends JFrame {
     //main method that creates GUI object
     public static void main(String[] args) {
         try {
-            SwingUtilities.invokeLater(() -> {
-                GUI frame = new GUI();
-                frame.setVisible(true);
+            CompletableFuture<Void> guiFuture = CompletableFuture.runAsync(() -> {
+                SwingUtilities.invokeLater(() -> {
+                    GUI frame = new GUI();
+                    frame.setVisible(true);
+                });
             });
-        }
-        catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "An error occurred while initializing the GUI");
-            main(args);
+
+            CompletableFuture<Void> gtfsLoaderFuture = CompletableFuture.runAsync(GTFSLoader::loadGraph);
+
+            CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(guiFuture, gtfsLoaderFuture);
+            combinedFuture.join();  // Wait for both tasks to complete
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "An error occurred while initializing the GUI or loading the graph");
         }
     }
 
